ce8635bc26f5b9c9a026802c887ad13432784afe
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2025-09-16 16:50:49 +0100

    Fonts

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # FrameworkV0
 The Themium Framework (Version 0)
+
+
+# Credits
+Here are all the credits for V0 (So far)
+
+## Fonts
+- Google Fonts
---
+++ index.js
@@ -0,0 +1,6 @@
+// Link Stylesheet
+const fw_sylesheet_link_2218 = document.createElement("link");
+fw_sylesheet_link_2218.rel = "stylesheet";
+fw_sylesheet_link_2218.type = "text/css";
+fw_sylesheet_link_2218.href = "https://themium.github.io/FrameworkV0/v0.css";
+document.head.appendChild(fw_sylesheet_link_2218);
